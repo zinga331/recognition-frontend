0f42c2cb3115c2c97f855a505b25da2c2e2ab768
Lawry Sorenson <lawrysorenson@gmail.com>
2023-10-27 11:52:02 -0600

    Add notification support

--- lawry-startup-css/index.js
+++ lawry-startup-css/index.js
@@ -1,11 +1,16 @@
 
 let loggedIn = false;
 
-// Display logged in user
 function init() {
     document.getElementById("loginLink").addEventListener("click", logout);
     renderLogin();
+
+    document.querySelector(".toggle-popup").addEventListener("click", showNotice);
+    document.querySelector(".notification").addEventListener("focusout", hideNotice);
+    setupWebsocket();
 }
+
+// Login Support
 
 function renderLogin() {
     let username = localStorage.getItem("username");
@@ -28,6 +33,35 @@ function logout() {
     } else {
         window.location.href = "login.html";
     }
+}
+
+// WebSocket Notifications
+
+let hideNoticeTimer;
+function setupWebsocket() {
+    // TODO
+    setTimeout(() => notify("You've been on the page for ten seconds"), 10000);
+}
+
+function notify(text) {
+    let notice = document.querySelector(".notification");
+    notice.textContent = text;
+    notice.style.display = 'block';
+    
+    if (hideNoticeTimer) clearTimeout(hideNoticeTimer),hideNoticeTimer=null;
+    hideNoticeTimer = setTimeout(hideNotice, 6000);
+}
+
+function showNotice() {
+    let notice = document.querySelector(".notification");
+    notice.style.display = 'block';
+    notice.focus();
+}
+
+function hideNotice() {
+    if (hideNoticeTimer) clearTimeout(hideNoticeTimer),hideNoticeTimer=null;
+    let notice = document.querySelector(".notification");
+    notice.style.display = 'none';
 }
 
 window.addEventListener("load", init);
