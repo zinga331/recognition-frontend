d5381d2c29fe6730cd567dc5e870522a61bb0cd6
Lawry Sorenson <lawrysorenson@gmail.com>
2023-11-09 11:15:44 -0700

    Call service endpoints

--- lawry-startup-javascript/login.js
+++ lawry-startup-javascript/login.js
@@ -1,9 +1,22 @@
 
-function login() {
+async function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    localStorage.setItem("username", username);
-    window.location.href = "index.html";
+    
+    let res = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: username, password: password })
+    });
+    
+    res = await res.json();
+
+    if (res.username) {
+        localStorage.setItem("username", username);
+        window.location.href = "index.html";
+    }
 }
 
 function register() {
--- lawry-startup-javascript/service.js
+++ lawry-startup-javascript/service.js
@@ -1,80 +1,23 @@
-let types = [
-    {
-        id: "french",
-        display: "French 1820 Records"
-    },
-    {
-        id: "finnish",
-        display: "Finnish 1770 Records"
-    }
-];
-
-let records = ["example.png", "example2.png", "example3.png", "example4.png"];
-
-let fieldMock = [
-    [
-        {
-            field: "Person's Name",
-            value: "Jean-Luc"
-        },
-        {
-            field: "Record Date",
-            value: "07/01/1819"
-        },
-        {
-            field: "Record Location",
-            value: "Bellefontaine-les-Champs"
-        },
-        {
-            field: "Mother's Name",
-            value: "Marie-Claire"
-        },
-        {
-            field: "Father's Name",
-            value: "Pierre Dupont"
-        }
-    ],
-    [
-
-        {
-            field: "Person's Name",
-            value: "Isabelle Dubois"
-        },
-        {
-            field: "Record Date",
-            value: "14/03/1821"
-        },
-        {
-            field: "Record Location",
-            value: "Saint-Étienne-sur-Mer"
-        },
-        {
-            field: "Mother's Name",
-            value: "Sophie Martin"
-        },
-        {
-            field: "Father's Name",
-            value: "François Leblanc"
-        }
-    ]
-]
-
 async function getTypes() {
-    return types;
+    let ans = await fetch("/api/types");
+    return ans.json();
 }
 
 async function getRecord(type) {
-    if (type != 'french') return null;
-    let ri = Math.floor(Math.random() * records.length);
-    let fi = Math.floor(Math.random() * fieldMock.length);
-    return {
-        id: 'abcdef',
-        imageURL: records[ri],
-        fields: fieldMock[fi]
-    };
+    let ans = await fetch(`/api/record?type=${encodeURIComponent(type)}`);
+    ans = await ans.json();
+    if (ans.error) return null;
+    return ans;
 }
 
 async function submitRecord(record) {
+    await fetch("/api/record", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(record)
+    });
     console.log("Submitting record:", record);
 }
 
