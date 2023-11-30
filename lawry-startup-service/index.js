b3736de309a63d9b2477d9023a6c1716368196e2
Lawry Sorenson <lawrysorenson@gmail.com>
2023-11-30 13:18:16 -0700

    Add web socket stub

--- lawry-startup-service/index.js
+++ lawry-startup-service/index.js
@@ -163,7 +163,28 @@ async function logout() {
 let hideNoticeTimer;
 function setupWebsocket() {
     // TODO
-    setTimeout(() => notify("You've been on the page for ten seconds"), 10000);
+    // setTimeout(() => notify("You've been on the page for ten seconds"), 10000);
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    let socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    console.log(socket);
+    socket.onopen = (event) => {
+        console.log("Connected");
+        //   this.displayMsg('system', 'game', 'connected');
+    };
+    socket.onclose = (event) => {
+        console.log("Disconnected");
+        //   this.displayMsg('system', 'game', 'disconnected');
+    };
+    socket.onmessage = async (event) => {
+        console.log("Message");
+        const msg = JSON.parse(await event.data.text());
+        console.log(msg);
+    //   if (msg.type === GameEndEvent) {
+    //     this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+    //   } else if (msg.type === GameStartEvent) {
+    //     this.displayMsg('player', msg.from, `started a new game`);
+    //   }
+    };
 }
 
 function notify(text) {
