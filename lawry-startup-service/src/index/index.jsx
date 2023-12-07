d8e9eb39e38d4d5b55fc94dec4ee4107c1a5882a
Lawry Sorenson <lawrysorenson@gmail.com>
2023-12-07 15:17:29 -0700

    Add index login stubs

---
+++ lawry-startup-service/src/index/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Index() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>index displayed here</div>
+    </main>
+  );
+}
