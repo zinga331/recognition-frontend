3e8aae50c3b725a273360339777b027493f6fd32
Lawry Sorenson <lawrysorenson@gmail.com>
2023-12-07 15:07:16 -0700

    Add React app

---
+++ lawry-startup-service/src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return <div className='body'>App will display here</div>;
+}
