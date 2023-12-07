ddfcf9174338385b48788bd9014a98b7000bca39
Lawry Sorenson <lawrysorenson@gmail.com>
2023-12-07 15:41:47 -0700

    Add routing template

--- lawry-startup-service/src/app.jsx
+++ lawry-startup-service/src/app.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Index } from './index/index';
 
 export default function App() {
-  return <div className='body'>
-    <header>
-      <h1>Full Page Indexing</h1>
+  return <BrowserRouter>
+        <div className='body'>
+            <header>
+                <h1>Full Page Indexing</h1>
 
-      <span id="userDisplay">Hello, Anon!</span>
-      <span id="loginLink">Login / Logout</span>
-      <button className="toggle-popup">Notifications</button>
-      <div className="notification" tabindex="0">No notifications yet</div>
-    </header>
+                <span id="userDisplay">Hello, Anon!</span>
+                <span id="loginLink">Login / Logout</span>
+                <button className="toggle-popup">Notifications</button>
+                <div className="notification" tabindex="0">No notifications yet</div>
+            </header>
 
-    <main>App will display here</main>
+            <main>
+                <Routes>
+                    <Route 
+                        path='/'
+                        element={<Index/>}
+                        exact/>
+                    <Route 
+                        path='/login'
+                        element={<Login/>}
+                        exact/>
+                    <Route path='*' element={<NotFound />} />
+                </Routes>      
+            </main>
 
-    <footer>
-      <span class="authors">Lawry Sorenson and Christian Hall</span>
-      <a href="https://github.com/zinga331/startup/tree/main">GitHub</a>
-    </footer>
+            <footer>
+                <span class="authors">Lawry Sorenson and Christian Hall</span>
+                <a href="https://github.com/zinga331/startup/tree/main">GitHub</a>
+            </footer>
+        </div>
+    </BrowserRouter>;
+}
 
-    </div>;
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
