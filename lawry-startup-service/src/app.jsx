36720f2aec9ab3463371aefda4ea864f21131dbe
Lawry Sorenson <lawrysorenson@gmail.com>
2023-12-07 15:10:13 -0700

    Add header and footer

--- lawry-startup-service/src/app.jsx
+++ lawry-startup-service/src/app.jsx
@@ -3,5 +3,22 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-  return <div className='body'>App will display here</div>;
+  return <div className='body'>
+    <header>
+      <h1>Full Page Indexing</h1>
+
+      <span id="userDisplay">Hello, Anon!</span>
+      <span id="loginLink">Login / Logout</span>
+      <button className="toggle-popup">Notifications</button>
+      <div className="notification" tabindex="0">No notifications yet</div>
+    </header>
+
+    <main>App will display here</main>
+
+    <footer>
+      <span class="authors">Lawry Sorenson and Christian Hall</span>
+      <a href="https://github.com/zinga331/startup/tree/main">GitHub</a>
+    </footer>
+
+    </div>;
 }
