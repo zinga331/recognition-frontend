f71675a5a57fc3b70390ad0193543e95a2b60bb7
Lawry Sorenson <lawrysorenson@gmail.com>
2023-12-07 15:50:09 -0700

    Add Login html elements

--- lawry-startup-service/src/app.jsx
+++ lawry-startup-service/src/app.jsx
@@ -6,37 +6,36 @@ import { Login } from './login/login';
 import { Index } from './index/index';
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+    <BrowserRouter>
         <div className='body'>
             <header>
                 <h1>Full Page Indexing</h1>
 
                 <span id="userDisplay">Hello, Anon!</span>
-                <span id="loginLink">Login / Logout</span>
+                <NavLink id="loginLink" to='login'>Logout</NavLink> {/* if logged in */}
                 <button className="toggle-popup">Notifications</button>
                 <div className="notification" tabindex="0">No notifications yet</div>
             </header>
 
-            <main>
-                <Routes>
-                    <Route 
-                        path='/'
-                        element={<Index/>}
-                        exact/>
-                    <Route 
-                        path='/login'
-                        element={<Login/>}
-                        exact/>
-                    <Route path='*' element={<NotFound />} />
-                </Routes>      
-            </main>
+            <Routes>
+                <Route 
+                    path='/'
+                    element={<Index/>}
+                    exact/>
+                <Route 
+                    path='/login'
+                    element={<Login/>}
+                    exact/>
+                <Route path='*' element={<NotFound />} />
+            </Routes>      
 
             <footer>
                 <span class="authors">Lawry Sorenson and Christian Hall</span>
                 <a href="https://github.com/zinga331/startup/tree/main">GitHub</a>
             </footer>
         </div>
-    </BrowserRouter>;
+    </BrowserRouter>);
 }
 
 function NotFound() {
--- lawry-startup-service/src/login/login.jsx
+++ lawry-startup-service/src/login/login.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import '../app.css'; // TODO: local elements
 
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main>
+      <h1 className="welcome">Welcome</h1>
+
+      <div className="content">
+
+        <div className="image-wrapper">
+          <img src="./images/example.png" className="image"></img>
+        </div>
+        
+        <div className="form-wrapper">
+          <form action="javascript:void(0);">
+            <input type="text" id="username" placeholder="Username" />
+            <input type="password" id="password" placeholder="Password" />
+            <div className="button-row">
+              <button id="loginButton">Login</button>
+              <button id="registerButton">Register</button>
+            </div>
+          </form>
+        </div>
+
+      </div>
     </main>
   );
 }
