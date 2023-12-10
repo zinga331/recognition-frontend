efab0cc0d877cd61a189bab964c8db6a8b3e629c
Christian Hall <christianjameshall@gmail.com>
2023-12-09 20:25:21 -0700

    Tenatively added login html to login jsx -- buttons don't work

--- chris-startup/src/login/login.jsx
+++ chris-startup/src/login/login.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
 
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main className="container-fluid bg-secondary text-center">
+      <body>
+        <div id="login">
+          <p>
+            <label for="username">Username: </label>
+            <input type="username" id="username" name="varUsername" />
+          </p>
+          <p>
+            <label for="password">Password: </label>
+            <input type="password" id="password" name="varPassword" />
+          </p>
+          <div className="button-container">
+            <button id="submit-button" type="submit">
+              Submit
+            </button>
+            <button id="register-button" type="register">
+              Register
+            </button>
+          </div>
+        </div>
+        <img
+          id="slideshow"
+          alt="Slideshow Placeholder"
+          src="images/census_example.png"
+          width="800px"
+        />
+        <script src="login.js"></script>
+      </body>
     </main>
   );
 }
